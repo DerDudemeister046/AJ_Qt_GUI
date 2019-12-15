39f3dd1a0655fc8ec91662f2ff61df7418711b7a
Andi <andi@stux-haven.de>
2019-12-15 16:33:39 +0100

    Imported old functions
    from old project, needs to be reworked.

--- CiderPress_AJ_GUI/corecommunicator.cpp
+++ CiderPress_AJ_GUI/corecommunicator.cpp
@@ -4,3 +4,54 @@ CoreCommunicator::CoreCommunicator(QObject *parent) : QObject(parent)
 {
 
 }
+
+void CoreCommunicator::start()
+{
+    QNetworkAccessManager *man = new QNetworkAccessManager(this);
+    connect(man, &QNetworkAccessManager::finished, this, &CoreCommunicator::replyFinished);
+    // url should constantly be updated!
+    const QUrl url = QUrl("http://192.168.0.2:9851/xml/modified.xml?password=25d55ad283aa400af464c76d713c07ad&session=1945950535&timestamp=1576089551879&filter=down;uploads;user;server;search;informations;ids");
+    QNetworkRequest request(url);
+    man->get(request);
+}
+
+
+QByteArray CoreCommunicator::getXMLReply()
+{
+    return xmlreply;
+}
+
+void CoreCommunicator::setXMLReply(QByteArray qa)
+{
+    this->xmlreply = qa;
+}
+
+void CoreCommunicator::write()
+{
+    QFile file("./test.xml"); // change to different dir/name
+    if (!file.open(QIODevice::WriteOnly | QIODevice::Text))
+    {
+        qDebug() << "Failed to open file";
+    }
+    else
+    {
+        QTextStream stream(&file);
+        stream << document.toString();
+        file.close();
+        qDebug() << "Finished!";
+    }
+}
+
+void CoreCommunicator::replyFinished(QNetworkReply *reply)
+{
+    setXMLReply(reply->readAll());
+    if (document.setContent(getXMLReply()))
+    {
+        qDebug() << "Success";
+        write();
+    }
+    else
+    {
+        qDebug() << "Failed!";
+    }
+}
--- CiderPress_AJ_GUI/corecommunicator.h
+++ CiderPress_AJ_GUI/corecommunicator.h
@@ -2,16 +2,28 @@
 #define CORECOMMUNICATOR_H
 
 #include <QObject>
+#include <QtXml>
+#include <QNetworkAccessManager> // Make requests to core
+#include <QNetworkReply> // handle core replies
+#include <QDebug>
 
 class CoreCommunicator : public QObject
 {
     Q_OBJECT
 public:
     explicit CoreCommunicator(QObject *parent = nullptr);
-
+    void start();
+    QByteArray getXMLReply();
+    void setXMLReply(QByteArray qa);
+    void write();
+private:
+    QByteArray xmlreply;
+    QDomDocument document;
 signals:
 
 public slots:
+private slots:
+    void replyFinished(QNetworkReply*);
 };
 
 #endif // CORECOMMUNICATOR_H
