98a6cea2f4c3548dcf6b2d717a84790bac80e2fb
Andreas Amzoll <amzoll.andreas@googlemail.com>
2019-12-17 09:15:57 +0100

    Class XML_Manager added
    new way of getting, handling, and processing XML Data from Core

--- CiderPress_AJ_GUI/main.cpp
+++ CiderPress_AJ_GUI/main.cpp
@@ -1,15 +1,15 @@
 #include "ciderpress.h"
 
 #include <QApplication>
-#include "corecommunicator.h"
-#include "xmlinterpreter.h"
 
+//#include "corecommunicator.h"
+//#include "xmlinterpreter.h"
 /* Test Includes */
-#include <QFile>
-#include <QtXml>
-#include "downloads_widget.h"
-#include "uploads_widget.h"
-#include "servers_widget.h"
+//#include <QFile>
+//#include <QtXml>
+//#include "downloads_widget.h"
+//#include "uploads_widget.h"
+//#include "servers_widget.h"
 
 
 #include <QDebug>
@@ -17,23 +17,18 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    //CiderPress w;
-    //w.show();
+    CiderPress w;
+    w.show();
 
 
-    Downloads_Widget *dw = new Downloads_Widget;
-    dw->show();
-
-    Uploads_Widget *uw = new Uploads_Widget;
-    uw->show();
-
-    Servers_Widget *sw = new Servers_Widget;
-    sw->show();
-
-
+    //Downloads_Widget *dw = new Downloads_Widget;
+    //dw->show();
+    //Uploads_Widget *uw = new Uploads_Widget;
+    //uw->show();
+    //Servers_Widget *sw = new Servers_Widget;
+    //sw->show();
     //CoreCommunicator* cc = new CoreCommunicator;
     //cc->start();
-
     //qDebug() << cc->urlCreator();
 
     return a.exec();
---
+++ CiderPress_AJ_GUI/xml_manager.cpp
@@ -0,0 +1,6 @@
+#include "xml_manager.h"
+
+XML_Manager::XML_Manager(QObject *parent) : QObject(parent)
+{
+
+}
